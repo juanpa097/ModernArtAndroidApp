632dc7aaa1ba490e2525397e7b3403cee91fe10b
Juan Pablo Peñaloza Botero <juan-penaloza@javeriana.edu.co>
2016-12-11 01:27:39 -0500

    Finished App

---
+++ app/src/main/java/co/edu/javeriana/acm/modernartui/ArtComposer.java
@@ -0,0 +1,77 @@
+package co.edu.javeriana.acm.modernartui;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.net.Uri;
+import android.preference.DialogPreference;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telecom.Call;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+
+public class ArtComposer extends AppCompatActivity {
+
+    private static final String TAG = "Debug";
+    private SeekBar mColorBar;
+
+    private RectangleOneFragment mArtFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_art_composer);
+
+        userInterfaceSetUp();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.activity_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Log.d("TAG", item.toString());
+        Log.d("TAG", "ENTER");
+        AlertDialog.Builder alert = new AlertDialog.Builder(ArtComposer.this);
+        alert.setTitle("See More Information");
+        alert.setMessage("Are you sure you whant to exit?");
+        alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                String url = "https://www.moma.org/";
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+        alert.setNegativeButton("No",null);
+        AlertDialog alertDialog = alert.create();
+        alertDialog.show();
+        return true;
+    }
+
+    private void userInterfaceSetUp() {
+        mColorBar = (SeekBar) findViewById(R.id.colorBar);
+        mArtFragment = (RectangleOneFragment) findViewById(R.id.art_fragment);
+        mColorBar.setOnSeekBarChangeListener(mArtFragment.getInstaceOfCollorController());
+    }
+
+
+
+
+}
---
+++ app/src/main/java/co/edu/javeriana/acm/modernartui/RectangleOneFragment.java
@@ -0,0 +1,114 @@
+package co.edu.javeriana.acm.modernartui;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.SeekBar;
+
+/**
+ * Created by JuanPablo on 12/10/16.
+ */
+
+public class RectangleOneFragment extends View {
+
+    private Paint mPaintRect1;
+    private Paint mPaintRect2;
+    private Paint mPaintRect3;
+    private Paint mPaintRect4;
+    private Rect mRect1;
+    private Rect mRect2;
+    private Rect mRect3;
+    private Rect mRect4;
+
+    public RectangleOneFragment (Context context) {
+        super(context);
+        init(null,0);
+    }
+
+    public RectangleOneFragment (Context context, AttributeSet attrs) {
+        super(context,attrs);
+        init(attrs,0);
+    }
+
+    public RectangleOneFragment (Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(null,defStyle);
+    }
+
+    private void init (AttributeSet attrs, int defStyle) {
+        mPaintRect1 = new Paint();
+        mPaintRect2 = new Paint();
+        mPaintRect3 = new Paint();
+        mPaintRect4 = new Paint();
+
+        mPaintRect1.setColor(Color.rgb(0,136,203));
+        mPaintRect2.setColor(Color.rgb(163,0,92));
+        mPaintRect3.setColor(Color.rgb(200,0,0));
+        mPaintRect4.setColor(Color.rgb(42,139,0));
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        int margin = canvas.getWidth() / 50;
+        int floorPadding = canvas.getHeight() / 20;
+
+        mRect1 = new Rect();
+        mRect1.set(0,0,canvas.getWidth()/3, canvas.getHeight()/2);
+
+        mRect2 = new Rect();
+        mRect2.set((canvas.getWidth()/3) + margin,0, canvas.getWidth() , canvas.getHeight()/4);
+
+        mRect3 = new Rect();
+        mRect3.set(0, canvas.getHeight()/2 + margin,canvas.getWidth()/3, canvas.getHeight() - floorPadding);
+
+        mRect4 = new Rect();
+        mRect4.set((canvas.getWidth()/3) + margin, canvas.getHeight()/4 + margin, canvas.getWidth() , canvas.getHeight() - floorPadding);
+
+        canvas.drawRect(mRect1,mPaintRect1);
+        canvas.drawRect(mRect2,mPaintRect2);
+        canvas.drawRect(mRect3,mPaintRect3);
+        canvas.drawRect(mRect4,mPaintRect4);
+    }
+
+    public ColorController getInstaceOfCollorController() {
+        return new ColorController();
+    }
+
+    private class ColorController implements SeekBar.OnSeekBarChangeListener {
+
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+            double increase = 255.0 * (Double.valueOf(i) / 100.00);
+
+            i = Double.valueOf(increase).intValue();
+            mPaintRect1.setColor(Color.rgb(i,136,203));
+            mPaintRect2.setColor(Color.rgb(163,i,92));
+            mPaintRect3.setColor(Color.rgb(200,i,i));
+            mPaintRect4.setColor(Color.rgb(42,139,i));
+            Log.d("TAG", String.valueOf(i));
+            invalidate();
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
+    }
+}
